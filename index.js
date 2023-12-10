aa876cede99d909b9de87ab9bd4a69577f5d9474
Aakashjha17 <80976345+Aakashjha17@users.noreply.github.com>
2023-12-10 16:01:27 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/:movieName', async (req,res) => {
     try {
         const movieDetails = await getMovieRatingsAndReview(req.params.movieName);
         const { Title, imdbRating, Review } = movieDetails;
-        const responseString = `Title: ${Title}\nRating: ${imdbRating}\nReview: ${Review}`;
+        const responseString = `Title: ${Title}\nRating: ${imdbRating}\nReview:\n ${Review}`;
         res.send(responseString);
     } catch (error) {
         res.status(500).send(`Error: ${error.message}`);
